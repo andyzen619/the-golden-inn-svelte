67d9fb8dbfce4885a3204b27c3a0105750c762ae
Andy Liang <andyzen619@gmail.com>
2023-12-12 06:11:08 +0000

    - set correct overflow for mobile page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
 	import { yearsSinceStartYear } from '$lib';
 
-	let mainBackgroundImageUrl = `https://firebasestorage.googleapis.com/v0/b/the-golden-inn-restaurant.appspot.com/o/goldenInnBackground.png?alt=media&token=c031b198-7ddc-4881-94e9-b61866bc15ca`;
-
 	let numberOfYearsInOperation = yearsSinceStartYear(1997);
 
 	const daysOfTheWeek = [
@@ -16,11 +14,13 @@
 	];
 </script>
 
-<div class="h-screen w-screen bg-[url('{mainBackgroundImageUrl}')]">
+<div
+	class="overflow-auto h-screen bg-[url('https://firebasestorage.googleapis.com/v0/b/the-golden-inn-restaurant.appspot.com/o/goldenInnBackground.png?alt=media&token=c031b198-7ddc-4881-94e9-b61866bc15ca')]"
+>
 	<!-- Landing Component -->
 	<div class="flex flex-col justify-center text-white h-2/3">
 		<div class="flex justify-center p-8">
-			<div class="px-6 py-24 rounded-full text-4xl opacity-75 bg-red-600">
+			<div class="px-6 py-20 rounded-full text-3xl opacity-75 bg-red-600">
 				<div class="flex justify-center text-white opacity-100">GOLDEN INN</div>
 				<div class="flex justify-center pt-2 opacity-100">RESTAURANT</div>
 			</div>
@@ -29,12 +29,10 @@
 			SERVING AUTHENTIC CANTONESE STYLE CHINESE FOOD FOR {numberOfYearsInOperation} YEARS
 		</div>
 	</div>
-
 	<!-- Visit US Component -->
 	<div class="flex flex-col w-screen justify-evenly text-white">
 		<div class="flex justify-center text-5xl">VISIT US</div>
-
-		<div class="flex justify-center text-xl text-center p-4">
+		<div class="flex justify-center text-xl text-center">
 			155 Main Street West, Picton, Prince Edward, ON, Canada
 		</div>
 		<div class="flex justify-center text-xl">613-476-7056</div>
@@ -48,11 +46,12 @@
 			</a>
 		</div>
 		<div id="/visit_us" class="flex justify-center text-xl">Hours of Operation</div>
-
-		<div class="flex justify-center text-lg">
-			<div class="flex flex-col justify-center">DAY</div>
-			<div class="w-12" />
-			<div class="flex flex-col justify-center">HOUR</div>
-		</div>
+		{#each daysOfTheWeek as item}
+			<div class="flex justify-center text-lg">
+				<div class="flex flex-col justify-center">{item.day}</div>
+				<div class="w-12" />
+				<div class="flex flex-col justify-center">{item.hours}</div>
+			</div>
+		{/each}
 	</div>
 </div>
